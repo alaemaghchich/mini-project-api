2e94235baa58859ceec06d83d2235e233668b8b0
ALAE MAGHCHICH <alaemaghchich2004@gmail.com>
2025-11-04 22:53:02 +0100

    api

--- cataloge de produit via api/main.js
+++ cataloge de produit via api/main.js
@@ -1,21 +1,14 @@
 const btn = document.querySelector(".search button");
 const search = document.querySelector(".search input");
 const radios = document.querySelectorAll('input[type="radio"]');
-const lang = document.getElementById("lang");
-let products = [];
 
-const tarjama = {
-  en: ["All Products", "Best Seller", "Fashion", "jewelery", "Electronics"],
-  fr: ["Tous les produits", "Meilleure vente", "Mode", "bijoux", "Électronique"],
-  ar: ["كل المنتجات", "الأكثر مبيعًا", "الأزياء", "المجوهرات", "الإلكترونيات"],
-};
+let products = [];
 
 fetch("./data.json")
   .then(res => res.json())
   .then(data => {
     products = data.products;
     show(products);
-    console.log(products);
   });
 
 function show(list) {
@@ -23,7 +16,7 @@ function show(list) {
   container.innerHTML = "";
   list.forEach(p => {
     const div = document.createElement("div");
-    div.classList.add("product");
+    div.className = "product";
     div.innerHTML = `
       <img src="${p.image}" width="100" height="100">
       <h3>${p.name}</h3> 
@@ -35,8 +28,7 @@ function show(list) {
 
 btn.addEventListener("click", () => {
   const text = search.value.toLowerCase();
-  show(products.filter(p => p.name.toLowerCase().includes(text)));
-});
+  show(products.filter(p => p.name.toLowerCase().includes(text)))});
 
 search.addEventListener("keypress", e => {
   if (e.key === "Enter") btn.click();
@@ -46,17 +38,10 @@ radios.forEach(radio => {
   radio.addEventListener("change", () => filterproducts());
 });
 
-function longlivalue(value) {
-  const flatLabels = Object.values(tarjama).flat();
-  const index = flatLabels.indexOf(value);
-  if (index === -1) return value;
-  return tarjama.en[index % 5];
-}
-
 function filterproducts() {
-  const selected = longlivalue(Array.from(radios).find(r => r.checked).value).toLowerCase();
-  let filtered = [];
+  const selected = Array.from(radios).find(r => r.checked).value.toLowerCase();
 
+  let filtered = [];
   if (selected === "all products") {filtered = products;}
   else if (selected === "best seller") {filtered = products.filter(p => p.bestSeller);}
   else if (selected === "fashion") {filtered = products.filter(p => p.category === "fashion");}
@@ -65,15 +50,6 @@ function filterproducts() {
   else {filtered = products;}
   show(filtered);
 }
-lang.addEventListener("change", () => {
-  const language = lang.value;
-  search.placeholder = language === "fr" ? "Nom du produit" : language === "ar" ? "اسم المنتج" : "Product name";
-
-  radios.forEach((r, i) => {
-    r.value = tarjama[language][i];
-    r.nextElementSibling.textContent = tarjama[language][i];
-  });
-});
 const dark = document.getElementById('dark');
 dark.addEventListener('click', () => {
   document.body.classList.toggle('dark');
